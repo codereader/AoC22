6f73a1aff25b903206d27b1452d21b38d79b0106
Alexander Paul <alexander.paul@apus.at>
2022-12-01 21:52:08 +0100

    BÄM - just learned [-1] can be used as index in python, so immediately make use of it!

--- Day01/main.py
+++ Day01/main.py
@@ -1,13 +1,12 @@
 from util import *
 
 numbers = input_as_integers()
-i = 0
-sums = {}
+sums = [0]
 for number in numbers:
     if number is None:
-        i += 1
+        sums.append(0)
     else:
-        sums[i] = sums[i] + number if i in sums else number
-result = sorted(sums.values(), reverse=True)
+        sums[-1] += number
+result = sorted(sums, reverse=True)
 print(f'Part 1: {result[0]}')  # 70374
 print(f'Part 2: {sum(result[:3])}')  # 204610
