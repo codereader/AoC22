5aa657faef75ab01fefcc5f5a6f1ce3d438e7cc4
codereader <greebo@angua.at>
2022-12-02 08:33:47 +0100

    Use a lambda to generalise the runPart method

--- Day2/src/AdventOfCode/Day2.java
+++ Day2/src/AdventOfCode/Day2.java
@@ -3,36 +3,30 @@ package AdventOfCode;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Stream;
 
 public class Day2 {
 
 	public static void main(String[] args) {
 		var sequence = ReadFile("./input.txt");
 		
-		runPart1(sequence);
-		runPart2(sequence);
+		runPart(1, sequence, line -> new Round(line));
+		runPart(2, sequence, line -> new RoundWithRequiredOutcome(line));
 	}
 	
-	public static void runPart1(List<String> sequence)
+	public static void runPart(int part, List<String> sequence, Function<String, Round> roundCreator)
 	{
-		// Part 1
-		var rounds = sequence.stream().map(line -> new Round(line));
+		var rounds = sequence.stream().map(roundCreator);
 		
-		System.out.println(String.format("[Part1] Total Player Score: %d", 
+		System.out.println(String.format("[Part%d] Total Player Score: %d",
+				part,
 				rounds.map(r -> r.getRoundScore()).reduce(Integer::sum).get()));
 	}
 	
-	public static void runPart2(List<String> sequence)
-	{
-		// Part 2
-		var rounds = sequence.stream().map(line -> new RoundWithRequiredOutcome(line));
-		
-		System.out.println(String.format("[Part2] Total Player Score: %d", 
-				rounds.map(r -> r.getRoundScore()).reduce(Integer::sum).get()));
-	}
-
 	// Read the given text file into a list of strings
 	public static List<String> ReadFile(String path)
 	{
