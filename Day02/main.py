58f5824c870b4281f6607c46255ff9859ff03706
Alexander Paul <alexander.paul@apus.at>
2022-12-02 13:10:26 +0100

    Day 2, Part 2

--- Day02/main.py
+++ Day02/main.py
@@ -3,8 +3,12 @@ def solve():
 
     points_for_shape = {'X': 1, 'Y': 2, 'Z': 3}
     points_for_play = {'A Y': 6, 'A Z': 0, 'B Z': 6, 'B X': 0, 'C X': 6, 'C Y': 0, 'A X': 3, 'B Y': 3, 'C Z': 3}
-    points = sum((points_for_play[line] + points_for_shape[line[2]] for line in util.input_as_strings()))
-    print(f'Part 1: {points}')  # 12794
+    def calculate_points(game): return sum((points_for_play[line] + points_for_shape[line[2]] for line in game))
+
+    print(f'Part 1: {calculate_points(util.input_as_strings())}')  # 12794
+
+    to_play = {'A Y': 'X', 'A Z': 'Y', 'B Z': 'Z', 'B X': 'X', 'C X': 'Y', 'C Y': 'Z', 'A X': 'Z', 'B Y': 'Y', 'C Z': 'X'}
+    print(f'Part 2: {calculate_points(f"{line[0]} {to_play[line]}" for line in util.input_as_strings())}')  # 14979
 
 
 solve()
