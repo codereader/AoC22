1bb01a283d41639581d51655291322a1765486ee
Alexander Paul <alexander.paul@apus.at>
2022-12-03 12:01:39 +0100

    Day 3, Part 2

--- Day03/main.py
+++ Day03/main.py
@@ -1,9 +1,18 @@
 def solve():
     import util
 
-    def get_prio(first, second):
-        return next((ord(c)-38 if c.isupper() else ord(c)-96 for c in first if c in second))
-    print(f'Part 1: {sum((get_prio(s[:len(s)//2], s[len(s)//2:]) for s in util.input_as_strings()))}')  # 7980
+    def get_prio(first, second, third):
+        return next((ord(c)-38 if c.isupper() else ord(c)-96 for c in first if c in second and (third is None or c in third)))
+
+    # Part 1
+    input_list = util.input_as_strings()
+    print(f'Part 1: {sum((get_prio(s[:len(s)//2], s[len(s)//2:], None) for s in input_list))}')  # 7980
+
+    # Part 2
+    result = 0
+    while len(input_list) > 0:
+        result += get_prio(input_list.pop(0), input_list.pop(0), input_list.pop(0))
+    print(f'Part 2: {result}')  # 2881
 
 
 solve()
