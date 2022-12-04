fb3496093f45d79de0075ead2875970ec372dc86
Angua <birgit@angua.at>
2022-12-04 06:51:46 +0100

    Let the Pair class do the comparing work.

--- 04 Cleanup/Cleanup/Pair.cs
+++ 04 Cleanup/Cleanup/Pair.cs
@@ -12,5 +12,18 @@
             First = new Elf(pair[0]);
             Second = new Elf(pair[1]);
         }
+
+        internal bool RangesContained()
+        {
+            return (First.Min <= Second.Min && First.Max >= Second.Max)
+                || (First.Min >= Second.Min && First.Max <= Second.Max);
+        }
+
+        internal bool RangesOverlap()
+        {
+            return (First.Min >= Second.Min && First.Min <= Second.Max)
+                || (Second.Min >= First.Min && Second.Min <= First.Max);
+        }
+
     }
 }
--- 04 Cleanup/Cleanup/Program.cs
+++ 04 Cleanup/Cleanup/Program.cs
@@ -4,7 +4,6 @@ using System.Reflection;
 
 var input = ResourceUtils.GetDataFromResource(Assembly.GetExecutingAssembly(), $"Cleanup.input.txt");
 
-
 var pairCollection = new List<Pair>();
 
 // 77-81,78-97
@@ -13,15 +12,12 @@ foreach (var line in input)
     pairCollection.Add(new Pair(line));
 }
 
-Console.WriteLine(pairCollection.Count);
-
 // part 1
-var count = pairCollection.Count(p => (p.First.Min <= p.Second.Min && p.First.Max >= p.Second.Max)
-        || (p.First.Min >= p.Second.Min && p.First.Max <= p.Second.Max));
+var count = pairCollection.Count(p => p.RangesContained());
 
 Console.WriteLine(count);
 
 // part 2
-var count2 = pairCollection.Count(p => (p.First.Range.Intersect(p.Second.Range)).Any());
+var count2 = pairCollection.Count(p => p.RangesOverlap());
 
 Console.WriteLine(count2);
