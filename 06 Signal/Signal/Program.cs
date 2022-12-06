49427f0feb75292e98b0b898321f77d437e5e048
Angua <birgit@angua.at>
2022-12-06 06:46:31 +0100

    Day 6 - Signal

---
+++ 06 Signal/Signal/Program.cs
@@ -0,0 +1,39 @@
+﻿using Common;
+using System.Net.Http.Headers;
+using System.Reflection;
+
+var input = ResourceUtils.GetDataFromResource(Assembly.GetExecutingAssembly(), $"Signal.input.txt");
+var testStr = input[0].ToList();
+
+var startPos = 0;
+var packageStart = -1;
+var messageStart = -1;
+
+while (true)
+{
+    // 4 different characters
+    if (testStr.GetRange(startPos, 4).Distinct().Count() == 4)
+    {
+        if (packageStart == -1)
+        {
+            packageStart = startPos + 4;
+        }
+    }
+
+    // 14 different characters
+    if (testStr.GetRange(startPos, 14).Distinct().Count() == 14)
+    {
+        messageStart = startPos + 14;
+        break;
+    }
+
+    startPos++;
+}
+
+Console.WriteLine("Start of package:");
+Console.WriteLine(packageStart);
+
+Console.WriteLine("Start of message:");
+Console.WriteLine(messageStart);
+
+Console.ReadLine();
