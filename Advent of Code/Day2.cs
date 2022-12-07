ab2676c2dc1067d9bc85dd1968d44df156a5b4a3
Kathrin Hubmann <kathrin.hubmann@apus.at>
2022-12-07 15:04:30 +0100

    Day 2 and 3

---
+++ Advent of Code/Day2.cs
@@ -0,0 +1,90 @@
+﻿namespace AdventOfCode_2
+{
+	using System;
+	using System.Collections.Generic;
+	using System.IO;
+
+	internal class Day2
+	{
+		public static void Main()
+		{
+			List<string> content = ReadFile("C:\\Users\\KathrinHubmann\\source\\repos\\AdventOfCode_2\\AdventOfCode_2\\strategy.txt");
+			int sum = 0;
+			foreach (var line in content)
+			{
+				string[] parts = line.Split();
+				sum += Score(parts[0], parts[1]);
+			}
+			Console.WriteLine(sum);
+			Console.ReadLine();
+		}
+
+		private static int Score(string opponent, string me)
+		{
+			switch (opponent)
+			{
+				case "A":
+					switch (me)
+					{
+						case "X":
+							return 3;
+
+						case "Y":
+							return 1 + 3;
+
+						case "Z":
+							return 2 + 6;
+
+						default: 
+							return 0;
+					}
+
+				case "B":
+					switch (me)
+					{
+						case "X":
+							return 1;
+
+						case "Y":
+							return 2 + 3;
+
+						case "Z":
+							return 3 + 6;
+
+						default:
+							return 0;
+					}
+
+				case "C":
+					switch (me)
+					{
+						case "X":
+							return 2;
+
+						case "Y":
+							return 3 + 3;
+
+						case "Z":
+							return 1 + 6;
+
+						default:
+							return 0;
+					}
+			}
+
+			return 0;
+		}
+
+		public static List<string> ReadFile(string file)
+		{
+			string[] fileContent = File.ReadAllLines(file);
+			List<string> strategy = new List<string>();
+
+			foreach (var line in fileContent)
+			{
+				strategy.Add(line);
+			}
+			return strategy;
+		}
+	}
+}
---
+++ Advent of Code/Day3.cs
@@ -0,0 +1,109 @@
+﻿namespace AdventOfCode_2
+{
+	using System;
+	using System.Collections.Generic;
+	using System.Diagnostics.CodeAnalysis;
+	using System.Linq;
+
+	internal class Day3
+	{
+
+		public static void Main()
+		{
+			List<string> content = Day2.ReadFile("contents.txt");
+			var list = Day2.ReadFile("letterToNumber.txt");
+			Dictionary<string, int> letterToNumber = MakeDictionary(list);
+
+			int sum1 = Duplicates(content, letterToNumber);
+			int sum2 = Groups(content, letterToNumber);
+			
+			Console.WriteLine(sum1);
+			Console.WriteLine(sum2);
+			Console.ReadLine();
+		}
+
+		private static Dictionary<string, int> MakeDictionary(List<string> list)
+		{
+			Dictionary<string, int> letterToNumber = new Dictionary<string, int>();
+			foreach (var text in list)
+			{
+				var split = text.Split(' ');
+				letterToNumber.Add(split[0], int.Parse(split[1]));
+			}
+			return letterToNumber;
+		}
+
+		private static int Duplicates(List<string> content, Dictionary<string, int> letterToNumber)
+		{
+			int sum = 0;
+			foreach (var rucksack in content)
+			{
+				var part1 = rucksack.Substring(0, rucksack.Length / 2);
+				var part2 = rucksack.Substring(rucksack.Length / 2, rucksack.Length / 2);
+
+				foreach (var letter1 in part1.ToCharArray().Distinct())
+				{
+					if (part2.ToCharArray().Contains(letter1))
+					{
+						int value = letterToNumber[letter1.ToString()];
+						sum += value;
+						break;
+					}
+				}
+			}
+
+			return sum;
+		}
+
+		private static int Groups(List<string> content, Dictionary<string, int> letterToNumber)
+		{
+			int sum = 0;
+
+			List<string> groupList = new List<string>();
+			string groupStr = "";
+			var count = 0;
+			foreach (var rucksack in content)
+			{
+
+				if (count == 3)
+				{
+					count = 0;
+					groupList.Add(groupStr);
+				}
+				if (count == 0)
+				{
+					groupStr = rucksack + ";";
+					count++;
+				}
+				else
+				{
+					groupStr = groupStr + rucksack + ";";
+					count++;
+				}
+
+				if (rucksack.Equals(content.Last()))
+				{
+					groupList.Add(groupStr);
+				}
+			}
+
+			foreach (var group in groupList)
+			{
+				var split = group.Split(';');
+				var part1 = split[0];
+				var part2 = split[1];
+				var part3 = split[2];
+				foreach (var letter in part1.ToCharArray().Distinct())
+				{
+					if (part2.Contains(letter) && part3.Contains(letter))
+					{
+						int value = letterToNumber[letter.ToString()];
+						sum += value;
+					}
+				}
+			}
+
+			return sum;
+		}
+	}
+}
