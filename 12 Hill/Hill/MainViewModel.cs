26cb84c79bb6e457c7a135610a632dfb8e5ea027
Angua <birgit@angua.at>
2022-12-12 13:30:57 +0100

    char grid

--- 12 Hill/Hill/MainViewModel.cs
+++ 12 Hill/Hill/MainViewModel.cs
@@ -3,6 +3,7 @@ using CommonWPF;
 using HillLib;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -13,6 +14,8 @@ namespace Hill
     class MainViewModel : ViewModelBase
     {
         public Geographer Geo { get; set; } = new Geographer();
+
+        public ObservableCollection<VisualLocation> VisualLocations { get; set; } = new ObservableCollection<VisualLocation>();
 
         public int ShortestDist
         {
@@ -25,9 +28,18 @@ namespace Hill
             var input = ResourceUtils.GetDataFromResource(Assembly.GetExecutingAssembly(), @"Hill.input.txt");
 
             Geo.Parse(input);
-            Geo.FindConnections();
-            ShortestDist = Geo.ShortestDistanceToDestination();
+            CreateVisuals();
+            //Geo.FindConnections();
+            //ShortestDist = Geo.ShortestDistanceToDestination();
 
+        }
+
+        private void CreateVisuals()
+        {
+            foreach (var location in Geo.Heightmap)
+            {
+                VisualLocations.Add(new VisualLocation(location.Value));
+            }
         }
     }
 }
---
+++ 12 Hill/Hill/VisualLocation.cs
@@ -0,0 +1,25 @@
+﻿using HillLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hill
+{
+    public class VisualLocation
+    {
+        private Location _location;
+
+        public string Str => _location.Letter.ToString();
+
+        public int X => (int)_location.Position.X;
+        public int Y => (int)_location.Position.Y;
+
+        public VisualLocation(Location location)
+        {
+            _location = location;
+        }
+    }
+}
