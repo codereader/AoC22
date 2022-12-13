6aa1c833689ee5c16cbf8272f5eb108a0e8d5240
Alexander Paul <alexander.paul@apus.at>
2022-12-13 20:53:24 +0100

    Day 9, Part 2; working, but can be simplified

--- Day09/main.py
+++ Day09/main.py
@@ -32,9 +32,17 @@ def solve():
                     knot_moves = False if abs(knot_before[0] - current_knot[0]) <= 1 and abs(knot_before[1] - current_knot[1]) <= 1 else True
                     next_direction = ""
                     if knot_moves:
-                        if abs(delta_x) > 1:
+                        if abs(delta_x) > 1 and abs(delta_y) > 1:
+                            x = delta_x // abs(delta_x)
+                            y = delta_y // abs(delta_y)
+                            current_knot = (knot_before[0] - x, knot_before[1] - y)
+                            knots[i] = current_knot
+                            if i == number_of_knots - 1:
+                                tail_visited.append(f'{current_knot[0]},{current_knot[1]}')
+                            continue
+                        elif abs(delta_x) > 1:
                             next_direction = "R" if delta_x > 0 else "L"
-                        if abs(delta_y) > 1:
+                        elif abs(delta_y) > 1:
                             next_direction = "U" if delta_y > 0 else "D"
 
                         if next_direction == "L":
@@ -51,7 +59,7 @@ def solve():
         return tail_visited
 
     print(f'Part 1: {len(set(solve_with_number_of_knots(2)))}')  # 5513
-    print(f'Part 2: {len(set(solve_with_number_of_knots(10)))}')  # ?
+    print(f'Part 2: {len(set(solve_with_number_of_knots(10)))}')  # 2427
 
 
 solve()
