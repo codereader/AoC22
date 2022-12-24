957893faf20711ed89f6a6e0373ac7674d94f4d2
codereader <greebo@angua.at>
2022-12-24 06:37:18 +0100

    Day 19 part 1 improved

--- Day19/src/AdventOfCode/Day19.java
+++ Day19/src/AdventOfCode/Day19.java
@@ -1,6 +1,7 @@
 package AdventOfCode;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Stack;
 import java.util.stream.Collectors;
@@ -28,7 +29,7 @@ public class Day19
 			
 			var maxGeodes = runSimulation(robots, 24);
 			
-			System.out.println(String.format("BluePrint %d Quality Level = %d", bluePrint.Index, bluePrint.Index * maxGeodes));
+			System.out.println(String.format("BluePrint %d can produce %d Geodes max, has Quality Level = %d", bluePrint.Index, maxGeodes, bluePrint.Index * maxGeodes));
 			
 			totalQualityLevel += bluePrint.Index * maxGeodes;
 		}
@@ -43,11 +44,19 @@ public class Day19
 		var oreRobot = robots.stream().filter(r -> r.getMaterial() == Material.Ore).findFirst().get();
 		startingInventory.Robots.get(Material.Ore).add(oreRobot);
 
+		// The maximum clay and ore costs of all robots
+		var maxCost = new HashMap<Material, Integer>();
+		
+		maxCost.put(Material.Ore, robots.stream().mapToInt(r -> r.getOreCost()).max().getAsInt());
+		maxCost.put(Material.Clay, robots.stream().mapToInt(r -> r.getClayCost()).max().getAsInt());
+		maxCost.put(Material.Obsidian, robots.stream().mapToInt(r -> r.getObsidianCost()).max().getAsInt());
+		
 		// Stack of inventories to calculcate
 		var inventoriesToInvestigate = new Stack<Inventory>();
 		inventoriesToInvestigate.push(startingInventory);
 		
 		var finishedInventories = new ArrayList<Inventory>();
+		var bestGeodeProduction = 0;
 		
 		while (!inventoriesToInvestigate.isEmpty())
 		{
@@ -58,8 +67,25 @@ public class Day19
 			
 			if (inventory.Time == minutes)
 			{
-				// End of the time line, add to finished inventories
-				finishedInventories.add(new Inventory(inventory.Time, inventory.Previous));
+				// End of the time line, add to finished inventories if this is better
+				if (inventory.Materials.get(Material.Geode) > bestGeodeProduction)
+				{
+					finishedInventories.clear();
+					finishedInventories.add(inventory);
+					bestGeodeProduction = inventory.Materials.get(Material.Geode);
+				}
+				continue;
+			}
+			
+			var timeLeft = minutes - inventory.Time;
+			var geodeProduction = inventory.Robots.get(Material.Geode).size();
+			var theoreticalGeodeOutput = timeLeft * geodeProduction + // remaining production of the robots we have
+					(timeLeft * (timeLeft - 1) / 2) + 		 // what is produced if we added one geode robot per minute
+					inventory.Materials.get(Material.Geode); // what we have already
+			
+			if (theoreticalGeodeOutput <= bestGeodeProduction)
+			{
+				// It's not possible for this path to top the best production we know so far
 				continue;
 			}
 			
@@ -69,6 +95,13 @@ public class Day19
 			// Check each possible production scenario
 			for (var robot : robots)
 			{
+				var robotOutput = robot.getMaterial();
+				
+				if (robotOutput != Material.Geode && inventory.Robots.get(robotOutput).size() >= maxCost.get(robotOutput))
+				{
+					continue; // don't build any more robots than we can make use of
+				}
+				
 				if (inventory.hasMaterialsForRobot(robot))
 				{
 					var newState = new Inventory(inventory.Time + 1, inventory);
@@ -80,6 +113,7 @@ public class Day19
 		}
 
 		System.out.println(String.format("Calculated %d possibilities", finishedInventories.size()));
+		System.out.println(String.format("Best Geode Production: %d", bestGeodeProduction));
 		
 		return finishedInventories.stream().mapToInt(i -> i.Materials.get(Material.Geode).intValue()).max().getAsInt();
 	}
